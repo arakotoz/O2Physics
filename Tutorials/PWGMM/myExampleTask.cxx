978a5edd6a47d95c4f5213567018318c557610eb
Andry Rakotozafindrabe <arakotoz@cern.ch>
2023-11-07 18:52:18 +0100

    PWGMM milestone 1

--- Tutorials/PWGMM/myExampleTask.cxx
+++ Tutorials/PWGMM/myExampleTask.cxx
@@ -15,6 +15,7 @@
 
 #include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
+#include "Common/DataModel/TrackSelectionTables.h"
 
 using namespace o2;
 using namespace o2::framework;
@@ -23,19 +24,29 @@ struct myExampleTask {
   // Histogram registry: an object to hold your histograms
   HistogramRegistry histos{"histos", {}, OutputObjHandlingPolicy::AnalysisObject};
 
+  Configurable<int> nBinsPt{"nBinsPt", 100, "N bins in pT histo"};
+
   void init(InitContext const&)
   {
     // define axes you want to use
+    const AxisSpec axisCounter{1, 0, +1, ""};
     const AxisSpec axisEta{30, -1.5, +1.5, "#eta"};
+    const AxisSpec axisPt{nBinsPt, 0, 10, "p_{T}(GeV/c)"};
 
     // create histograms
+    histos.add("eventCounter", "eventCounter", kTH1F, {axisCounter});
     histos.add("etaHistogram", "etaHistogram", kTH1F, {axisEta});
+    histos.add("ptHistogram", "ptHistogram", kTH1F, {axisPt});
   }
 
-  void process(aod::TracksIU const& tracks)
+  void process(aod::Collision const& collision, soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksDCA> const& tracks)
   {
+    histos.fill(HIST("eventCounter"), 0.5);
     for (auto& track : tracks) {
+      if (track.tpcNClsCrossedRows() < 70 ) continue; //badly tracked
+      if (fabs(track.dcaXY()) > 0.2) continue; //doesn’t point to primary vertex
       histos.fill(HIST("etaHistogram"), track.eta());
+      histos.fill(HIST("ptHistogram"), track.pt());
     }
   }
 };
